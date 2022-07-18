8586d37ddf60bdd7fa57acc7518c42a98fd006c8
Developer-felix <onjombafelix1@gmail.com>
2022-07-18 15:24:19 +0300

    Tests

--- src/test/java/com/example/loanapp/PaymentMethodControllerTest.java
+++ src/test/java/com/example/loanapp/PaymentMethodControllerTest.java
@@ -2,7 +2,17 @@ package com.example.loanapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.sql.ResultSet;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class PaymentMethodControllerTest {
     @Autowired
@@ -10,10 +20,37 @@ public class PaymentMethodControllerTest {
 
     @Test
     public void testGetPaymentMethods() throws Exception {
-        mvc.perform(get("/api/v1/payment_methods"))
+        ResultActions paypal = mvc.perform(get("/api/v1/payment_method"))
                 .andExpect(status().isOk())
                 .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect((ResultMatcher) jsonPath("$.[0].name").value("Paypal"))
+                .andExpect((ResultMatcher) jsonPath("$.[0].name").value("Paypal"));
+    }
+
+    @Test
+    public void testGetPaymentMethodsByName() throws Exception {
+        ResultActions paypal = mvc.perform(get("/api/v1/payment_method/Paypal"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect((ResultMatcher) jsonPath("$.[0].name").value("Paypal"));
+    }
+
+    @Test
+    public void testGetPaymentMethodsByNameNotFound() throws Exception {
+        ResultActions paypal = mvc.perform(get("/api/v1/payment_method/Paypal"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect((ResultMatcher) jsonPath("$.[0].name").value("Paypal"));
+    }
+
+    @Test
+    public void testPostPaymentMethods() throws Exception{
+
+    }
+
+    @Test
+    public void searchPaymentMethods() throws Exception{
+        
+
     }
 
 
